da44b5f439bc0028daa64d9b673d9c202114a50d
Achen7068 <you@example.com>
2020-12-21 22:04:18 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./Componenets/HomePage/Home";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componenets/HomePage/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import styles from './Home.module.css';
+
+function Home() {
+    return (
+        <div className={styles.background}>
+            <div >
+                Alex Chen
+            </div>
+        </div>
+    )
+}
+export default Home;
